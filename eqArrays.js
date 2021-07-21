23dccd661a2d70d600e3152b067f2a55e23796f3
Stephanie <stephanie.grant.2008@gmail.com>
2021-07-21 14:26:54 +0000

    created eqArrays.js and fixed emojis in assertEqual.js

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion passed: ${actual} === ${expected}.`);
+  } else {
+    console.log(`❌❌❌Assertion failed: ${actual} !== ${expected}.`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    let checkTrue = true;
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        continue;
+      } else {
+        checkTrue = false;
+        break;
+      } 
+    } return checkTrue;
+  } else {
+    return false;
+  }  
+}
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
