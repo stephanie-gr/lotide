85a793c00c4553fc0cfc21a20a8764efccfc1a1d
Stephanie <stephanie.grant.2008@gmail.com>
2021-07-22 20:02:56 +0000

    created eqObject.js

---
+++ eqObject.js
@@ -0,0 +1,31 @@
+const eqObjects = function(object1, object2) {
+  let keys1 = Object.keys(object1);
+  let keys2 = Object.keys(object2);
+  if (keys1.length === keys2.length) {
+    for (let item of keys1) {
+      if (object1[item] !== object2[item]) {
+        return false;
+      }
+    } return true; 
+  } else {
+    return false;
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion passed: ${actual} === ${expected}.`);
+  } else {
+    console.log(`❌❌❌Assertion failed: ${actual} !== ${expected}.`);
+  }
+};
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); 
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);
+
+
