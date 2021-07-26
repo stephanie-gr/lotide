1ef5a3e19808a3862dd2251ba76c3b72f2639755
Stephanie <stephanie.grant.2008@gmail.com>
2021-07-26 19:24:33 +0000

    created test directory & assertEqualTest & headTest; removed test code from assertEqual.js and head.js

--- head.js
+++ head.js
@@ -1,17 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion passed: ${actual} === ${expected}.`);
-  } else {
-    console.log(`❌❌❌Assertion failed: ${actual} !== ${expected}.`);
-  }
-};
-
 const head = function(array) {
   let firstElement = array.shift();
   return firstElement;
 };
 
-assertEqual(head([5]), 5);
-assertEqual(head([]), "Hello");
-assertEqual(head([5,6,7]), 57);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), 10);
+module.exports = head;
+
---
+++ test/assertEqualTest.js
@@ -0,0 +1,4 @@
+
+const assertEqual = require('../assertEqual');
+
+assertEqual(12, 12);
